39f9aa4e5c7dcd9dbd7e4dd4ed474857428fc752
Peihan Gao <pg477@cornell.edu>
2021-05-30 11:49:11 +0800

    global version

--- goto.sh
+++ goto.sh
@@ -1,23 +1,36 @@
 #!/bin/bash
 
-typeset -l para
-para=$1
+abs_dir="${gptravel_abs}travel/"
 
-cache_flag=true
+typeset -l para
+cache_flag=false
+find_flag=false
+
+# Dir
+config_dir="${abs_dir}config"
+source $config_dir
+
+# Default dir for no command line parameters
+if [[ $# -eq 0 ]]
+then
+    para="default"
+else
+    para=$1
+fi
+
 
 function cache(){
-    local tag=`sed -n "1,1p" cache`
-    local address=`sed -n "2,2p" cache`
-    echo $tag $1
+    local tag=`sed -n "1,1p" $cache_dir`
+    local address=`sed -n "2,2p" $cache_dir`
     if [[ "$1" == "$tag" ]]
     then
         cd $address
-        cache_flag=false
+        cache_flag=true
     fi
 }
 
-function finddir(){
 
+function finddir(){
     local length=${#1}
     typeset -l address
     while read address
@@ -28,20 +41,31 @@ function finddir(){
             local st=`expr ${length} + 1`
             local num=`expr ${#address} - $length - 1`
             local mydir=${address:${st}:${num}}
+            
+            cd $mydir
+            find_flag=true
+            
             echo $1 > cache
             echo $mydir >> cache
+            
             break
         fi
-    done < config
-
-    cd $mydir
+    done < $list_dir
+    
 }
+
 
 cache $para
 
-if $cache_flag
+if [[ $cache_flag == false ]]
 then
     finddir $para
 fi
 
-unset para cache_flag
+if [[ $cache_flag == false && $find_flag == false ]]
+then
+    echo "\nNo such path found."
+    echo "Please check the command line parameter or add the target path to the list.\n"
+fi
+
+unset para cache_flag find_flag
